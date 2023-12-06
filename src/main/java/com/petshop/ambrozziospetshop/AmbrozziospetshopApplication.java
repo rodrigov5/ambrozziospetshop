243a91174f74e66c7fbfef617883c66f365d4dc8
Rodrigo Veloso <57206601+rodrigov5@users.noreply.github.com>
2023-12-06 01:02:01 -0300

    Initial commit

---
+++ src/main/java/com/petshop/ambrozziospetshop/AmbrozziospetshopApplication.java
@@ -0,0 +1,13 @@
+package com.petshop.ambrozziospetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbrozziospetshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbrozziospetshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petshop/ambrozziospetshop/AmbrozziospetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petshop.ambrozziospetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbrozziospetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
